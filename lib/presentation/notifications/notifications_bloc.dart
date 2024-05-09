3735642b8e7efdc2f652784a8ea4283a87ee227d
DIoads <jose.alejandro.guzman156@gmail.com>
2024-05-08 21:07:26 -0700

    cambios 07-05-2024

--- lib/presentation/notifications/notifications_bloc.dart
+++ lib/presentation/notifications/notifications_bloc.dart
@@ -1,16 +1,27 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_notification/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+
   NotificationsBloc() : super(const NotificationsState()) {
-    // on<NotificationsEvent>((event, emit) {
-    //   // TODO: implement event handler
-    // });
+    on<NotificationsStatusChanged>(_notificationsStatusChanged);
+  }
+
+  static Future<void> initializeFON() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
+
+  void _notificationsStatusChanged(
+      NotificationsStatusChanged event, Emitter<NotificationsState> emit) {
+    emit(state.copyWith(status: event.status));
   }
 
   void requestPermssion() async {
@@ -23,6 +34,6 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       provisional: false,
       sound: true,
     );
-    settings.authorizationStatus;
+    add(NotificationsStatusChanged(settings.authorizationStatus));
   }
 }
