c2b25ce80c5baa5856661bd83088736af59da4cb
DIoads <jose.alejandro.guzman156@gmail.com>
2024-05-10 19:22:01 -0700

    09-05-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:push_notification/config/router/app_router.dart';
 import 'package:push_notification/config/theme/app_theme.dart';
-import 'package:push_notification/presentation/providers/notifications/notifications_bloc.dart';
 import 'package:push_notification/presentation/providers/head_provider.dart';
+import 'package:push_notification/presentation/providers/notifications/notifications_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationsBloc.initializeFCM();
 
-  runApp(HeadProvider.initProvider(const MainApp()));
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  runApp(HeadProvider.initProvider(mainAppWidget: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
--- lib/presentation/providers/head_provider.dart
+++ lib/presentation/providers/head_provider.dart
@@ -3,14 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:push_notification/presentation/providers/notifications/notifications_bloc.dart';
 
 class HeadProvider {
-  static MultiBlocProvider initProvider(Widget mainApp) {
+  static MultiBlocProvider initProvider({required Widget mainAppWidget}) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
           create: (_) => NotificationsBloc(),
         ),
       ],
-      child: mainApp,
+      child: mainAppWidget,
     );
   }
 }
